fee81fa8fcc953391a89ce53f569d613ccc409fb
tzabcoder <60833046+tzabcoder@users.noreply.github.com>
2022-02-24 15:47:12 -0500

    Add files via upload

--- Block.h
+++ Block.h
@@ -7,17 +7,39 @@
 using namespace std;
 
 /*
-Block Structure:
-     Nonce => 64-bit signed integer 
-          - Used for the mining process
-     Hash => character string
-          - Holds the hash of the previous block
-     Index => 32-bit unsigned integer 
-          - Keeps track of the block number
-     Time => time integer
-          - Used for the block timestamp
-     Data => character string
-          - Holds a list of all of the transactions for that block
+% Block Structure:
+% Header:
+% - Hash (previous block)
+% - Timestamp 
+% - Nonce 
+% - Hash 
+% Body:
+% - Transaction List (Data)
+*/
+
+/*
+     Class Block:
+     public:
+          Constructor Block(nIndexIn, const &sDataIn)
+          - nIndexn => unsigned 32-bit integer
+          - &sDataIn => constant string reference
+
+          string sPrevHash
+          - String variable containing the previous blocks' hash
+
+          string GetHash()
+          
+          void MineBlock(nDifficulty)
+          - nDifficulty => unsigned 32-bit integer
+
+     private:
+          uint32_t _nIndex => 32-bit unsigned integer
+          int64_t _nNonce => 64-bit signed integer
+          string _sData => string 
+          string _sHash => string 
+          time_t _tTime => type time
+
+          string _CalculateHash() const
 */
 
 class Block {
@@ -25,19 +47,23 @@ public:
      //Constructor
      Block(uint32_t nIndexIn, const string &sDataIn);
 
+     //Public Variables
      string sPrevHash;
 
+     //Public Functions
      string GetHash();
      void MineBlock(uint32_t nDifficulty);
+
 private:
-     //Block Data
+     //Private Variables
      uint32_t _nIndex;
      int64_t _nNonce;
      string _sData;
      string _sHash;
      time_t _tTime;
 
-     string _CalculateHash();
+     //Private Functions
+     string _CalculateHash() const; //const directive ensures function cannot change variables in block class
 };
 
 #endif
